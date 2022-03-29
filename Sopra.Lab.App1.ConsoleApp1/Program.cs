0a2936929ac9c6a8b398acfb5287163f25147680
vPlata98 <platava98@gmail.com>
2022-03-29 16:50:22 +0200

    nuevo proyecto, aplicacion de consola

---
+++ Sopra.Lab.App1.ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sopra.Lab.App1.ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
